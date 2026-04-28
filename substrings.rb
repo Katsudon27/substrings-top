721b4bbab29d117a91493440737beda1dd8d5a39
Katsudon27 <katsudon1127@gmail.com>
2026-04-28 10:42:40 +1000

    Setting up for substring method

---
+++ substrings.rb
@@ -0,0 +1,6 @@
+def substrings(sentence, dictionary)
+
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("below", dictionary)
